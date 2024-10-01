81882be06f06d952c38d14bfb16c2535abc1d952
Minh Nguyen <minhnnguyen003@gmail.com>
2024-10-01 13:15:28 +1000

    fix: Fetch Criteria Endpoints SQL query bug

--- function_app.py
+++ function_app.py
@@ -3,13 +3,14 @@ import logging
 import mysql.connector
 import os
 from mysql.connector import errorcode
+import json
 
 dbConfig = {
     'host': os.environ['DB_HOST'],
     'database': os.environ['DB_DB'],
     'user': os.environ['DB_USR'],
     'password': os.environ['DB_PWD'],
-    'ssl_ca': 'DigiCertGlobalRootCA.pem',
+    'ssl_ca': './DigiCertGlobalRootCA.pem',
     'ssl_disabled' : False
 }
 
@@ -19,7 +20,7 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 def criteria(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    id = req.route_params.get('id')
+    id = req.route_params.get('skillId')
 
     try:
         conn = mysql.connector.connect(**dbConfig)
@@ -31,17 +32,18 @@ def criteria(req: func.HttpRequest) -> func.HttpResponse:
             print("Database does not exist")
         else:
             print(err)
+            print(**dbConfig)
     else:
-        cursor = conn.cursor()
+        cursor = conn.cursor(dictionary=True)
 
-    query = f"SELECT FROM TABLE WHERE skill_id = {id};"
+    query = f"select C.criteria_id, C.skill_level_id, S.level_id, C.criteria from Criteria as C inner join skill_level as S ON C.skill_level_id = S.skill_level_id WHERE skill_id = {id};"
 
     try:
         cursor.execute(query)
         result = cursor.fetchall()
         if result:
-            return func.HttpResponse(result, 200)
+            return func.HttpResponse(json.dumps(result), status_code=200)
         else:
-            return func.HttpResponse(None, 200)
+            return func.HttpResponse(None, status_code=200)
     finally:
         conn.close()
