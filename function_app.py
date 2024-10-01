0cdb2d85c6d6591387f6cd3c57ab956a06174c94
Minh Nguyen <minhnnguyen003@gmail.com>
2024-10-01 12:15:47 +1000

    feat: Fetch Criteria Endpoints

--- function_app.py
+++ function_app.py
@@ -45,35 +45,3 @@ def criteria(req: func.HttpRequest) -> func.HttpResponse:
             return func.HttpResponse(None, 200)
     finally:
         conn.close()
-
-
-@app.route(route="text_component/{skillId:int?}", methods=["GET"])
-def criteria(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    id = req.route_params.get('id')
-
-    try:
-        conn = mysql.connector.connect(**dbConfig)
-        logging.info("Connection Established")
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Something is wrong with the username or password")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database does not exist")
-        else:
-            print(err)
-    else:
-        cursor = conn.cursor()
-
-    query = f"SELECT FROM TABLE WHERE skill_id = {id};"
-
-    try:
-        cursor.execute(query)
-        result = cursor.fetchall()
-        if result:
-            return func.HttpResponse(result, 200)
-        else:
-            return func.HttpResponse(None, 200)
-    finally:
-        conn.close()
